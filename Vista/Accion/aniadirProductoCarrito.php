4ce3b1583bf68a71c79dc5456b62c55fcd9bcd31
Alexis Casimiro <exequielcasimiro@gmail.com>
2024-05-21 19:18:33 -0300

    encontrando la logica para agregar producto al carrito

--- Vista/Accion/aniadirProductoCarrito.php
+++ Vista/Accion/aniadirProductoCarrito.php
@@ -12,8 +12,13 @@ if (isset($datos['idproducto'])){
     //busco si el usuario no tiene compras
     $compraCliente=$abmCompra->buscar(['idusuario' => $idUsuario]);
     //si el usuario no tiene compras se crea una compra con el estado 5 (carrito)
-    if(count($compraCliente)==0){
-
+    //posible solucion no estoy seguro todavia=
+    $unaCompra=$compraCliente[count($compraCliente)-1];
+    if(count($compraCliente)==0 || count($abmCompraEstado->buscar(['idcompra' => $unaCompra->getIdCompra()])) != 1){
+        //creo el carrito si el cliente no tiene un objeto compra o si el cliente tiene compras pero no esta en estado carrito
+        echo "crear compra en estado carrito";
+    }else{
+        echo "existe una compra en estado carrito";
     }
 }
 ?>
